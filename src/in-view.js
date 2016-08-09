0bd2a11cd56df86bb810c4dea9d67bedad3fb5c5
Cam Wiegert <cam@camwiegert.com>
2016-08-08 20:40:52 -0500

    Move utils into main

--- src/in-view.js
+++ src/in-view.js
@@ -86,4 +86,27 @@ class inViewGroup {
 
 }
 
+export function inViewport(element, offset = 0) {
+    let bounds = element.getBoundingClientRect();
+    return bounds.bottom > offset
+        && bounds.right > offset
+        && window.innerWidth - bounds.left > offset
+        && window.innerHeight - bounds.top > offset;
+}
+
+export function throttle(fn, threshold, context) {
+    let prev = 0;
+    return () => {
+        let now  = new Date().getTime();
+        if (now - prev > threshold) {
+            fn.call(context);
+            prev = now;
+        }
+    };
+}
+
+export function getElements(selector) {
+    return [].slice.call(document.querySelectorAll(selector));
+}
+
 export default initInView();
