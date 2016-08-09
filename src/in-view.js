4e419e4dfdf36109e736ad3575a39363be2e7faa
Cam Wiegert <cam@camwiegert.com>
2016-08-09 10:36:58 -0500

    Move all functions into init

--- src/in-view.js
+++ src/in-view.js
@@ -2,21 +2,39 @@ import Registry from './registry';
 
 const initInView = () => {
 
+    const getElements = (selector) =>
+        [].slice.call(document.querySelectorAll(selector));
+
+    const inViewport = (element, offset = 0) => {
+        let bounds = element.getBoundingClientRect();
+        return bounds.bottom > offset
+            && bounds.right > offset
+            && window.innerWidth - bounds.left > offset
+            && window.innerHeight - bounds.top > offset;
+    };
+
+    const throttle = (fn, threshold, context) => {
+        let prev = 0;
+        return () => {
+            let now  = new Date().getTime();
+            if (now - prev > threshold) {
+                fn.call(context);
+                prev = now;
+            }
+        };
+    };
+
     let catalog = { history: [] };
 
     let inView = (selector) => {
-
         let elements = getElements(selector);
-
         if (catalog.history.indexOf(selector) > -1) {
             catalog[selector].elements = elements;
         } else {
             catalog[selector] = new Registry(elements);
             catalog.history.push(selector);
         }
-
         return catalog[selector];
-
     };
 
     inView.is = inViewport;
@@ -24,28 +42,5 @@ const initInView = () => {
     return inView;
 
 };
-
-function inViewport(element, offset = 0) {
-    let bounds = element.getBoundingClientRect();
-    return bounds.bottom > offset
-        && bounds.right > offset
-        && window.innerWidth - bounds.left > offset
-        && window.innerHeight - bounds.top > offset;
-}
-
-function throttle(fn, threshold, context) {
-    let prev = 0;
-    return () => {
-        let now  = new Date().getTime();
-        if (now - prev > threshold) {
-            fn.call(context);
-            prev = now;
-        }
-    };
-}
-
-function getElements(selector) {
-    return [].slice.call(document.querySelectorAll(selector));
-}
 
 export default initInView();
