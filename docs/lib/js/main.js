78f2b79e30f8ca18583152e8f2af8dd1a518aade
Cam Wiegert <cam@camwiegert.com>
2016-08-22 23:06:05 -0500

    Add dots to docs

--- docs/lib/js/main.js
+++ docs/lib/js/main.js
@@ -1,1 +1,24 @@
 import inView from '../../../src';
+
+let count = 150;
+let aside = document.querySelector('.aside');
+
+const createDot = () => {
+    let dot = document.createElement('div');
+    dot.className = 'dot';
+    dot.style.top = `${(Math.random() * 100)}%`;
+    dot.style.left = `${(Math.random() * 100)}%`;
+    return dot;
+}
+
+while (count--) {
+    aside.appendChild(createDot());
+}
+
+inView.offset(100);
+
+inView('.dot')
+    .on('enter', el =>
+        el.classList.add('in-view'))
+    .on('exit', el =>
+        el.classList.remove('in-view'));
