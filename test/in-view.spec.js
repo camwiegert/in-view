5c6e59cef7d0a4c729f7375b30072d73bbcf1244
Cam Wiegert <cam@camwiegert.com>
2016-08-30 13:25:22 -0500

    Add initial tests for inView interface

--- test/in-view.spec.js
+++ test/in-view.spec.js
@@ -1,3 +1,33 @@
 import test from 'ava';
+import inView from '../src/in-view';
+import Registry from '../src/registry';
 
-test.todo('Write test for inView');
+test('inView is a function', t => {
+    t.true(typeof inView === 'function');
+});
+
+test('inView returns a registry', t => {
+    t.true(inView('body').__proto__ === Registry([]).__proto__);
+});
+
+test('inView returns existing registries', t => {
+    let registry = inView('body');
+    t.true(registry === inView('body'));
+});
+
+test('inView updates existing registry elements', t => {
+
+    const addDiv = () => {
+        document.body.appendChild(
+            document.createElement('div')
+        );
+    };
+
+    t.true(inView('div').elements.length === 0);
+
+    addDiv();
+    t.true(inView('div').elements.length === 1);
+
+});
+
+test.after(initBrowserEnv);
