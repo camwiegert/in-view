5bd24035ec1c7af5af4b1dfcb6f19b886b05a18b
Cam Wiegert <cam@camwiegert.com>
2016-08-30 12:29:46 -0500

    Add initial tests for Registry.emit

--- test/registry.emit.spec.js
+++ test/registry.emit.spec.js
@@ -1,3 +1,42 @@
 import test from 'ava';
+import Registry from '../src/registry';
 
-test.todo('Write test for Registry.emit');
+test('Registry.emit calls each handler', t => {
+
+    let registry = Registry([]);
+
+    registry.on('enter', x => t.true(x === 'a'));
+    registry.on('enter', y => t.true(y === 'a'));
+
+    registry.on('exit', x => t.true(x === 'b'));
+    registry.on('exit', y => t.true(y === 'b'));
+
+    registry.once('enter', x => t.true(x === 'a'));
+    registry.once('enter', y => t.true(y === 'a'));
+
+    registry.once('exit', x => t.true(x === 'b'));
+    registry.once('exit', y => t.true(y === 'b'));
+
+    registry.emit('enter', 'a');
+    registry.emit('exit', 'b');
+
+});
+
+test('Registry.emit removes once handlers', t => {
+
+    let registry = Registry([]);
+
+    registry.once('enter', () => {});
+    t.true(registry.singles.enter.length === 1);
+
+    registry.emit('enter', {});
+    t.true(!registry.singles.enter.length);
+
+});
+
+test('Registry.emit returns the registry', t => {
+
+    let registry = Registry([]);
+    t.deepEqual(registry.emit('enter', {}), registry);
+
+});
