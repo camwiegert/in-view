1bc649248819bda6dce5297e97ec74f2958896eb
Cam Wiegert <cam@camwiegert.com>
2016-08-30 12:50:16 -0500

    Add initial tests for Registry.check

--- test/registry.check.spec.js
+++ test/registry.check.spec.js
@@ -1,3 +1,79 @@
 import test from 'ava';
+import Registry from '../src/registry';
 
-test.todo('Write test for Registry.check');
+window.innerWidth = 1280;
+window.innerHeight = 700;
+
+test('Registry.check updates current', t => {
+
+    let registry = Registry([{
+        getBoundingClientRect() {
+            return {
+                bottom: 1,
+                left: 1,
+                right: 1,
+                top: 1
+            };
+        }
+    }, {
+        getBoundingClientRect() {
+            return {
+                bottom: -1,
+                left: -1,
+                right: -1,
+                top: -1
+            };
+        }
+    }]);
+
+    t.true(!registry.current.length);
+
+    registry.check(0);
+    t.true(registry.current.length === 1);
+
+});
+
+test('Registry.check emits enter events', t => {
+
+    let stub = {
+        getBoundingClientRect() {
+            return {
+                bottom: 1,
+                left: 1,
+                right: 1,
+                top: 1
+            };
+        }
+    };
+
+    let registry = Registry([stub]);
+
+    registry.on('enter', el => t.deepEqual(el, stub));
+    registry.check(0);
+
+});
+
+test('Registry.check emits exit events', t => {
+
+    let stub = {
+        getBoundingClientRect() {
+            return {
+                bottom: -1,
+                left: -1,
+                right: -1,
+                top: -1
+            };
+        }
+    };
+
+    let registry = Registry([stub]);
+
+    registry.on('exit', el => t.deepEqual(el, stub));
+    registry.check(0);
+
+});
+
+test('Registry.check returns the registry', t => {
+    let registry = Registry([]);
+    t.deepEqual(registry.check(0), registry);
+});
