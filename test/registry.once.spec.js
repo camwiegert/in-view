90542e33c2fe97c289f4b95a3d6db1ea399a21ea
Cam Wiegert <cam@camwiegert.com>
2016-08-30 11:51:59 -0500

    Add initial tests for Registry.once

--- test/registry.once.spec.js
+++ test/registry.once.spec.js
@@ -1,3 +1,21 @@
 import test from 'ava';
+import Registry from '../src/registry';
 
-test.todo('Write test for Registry.once');
+test('Registry.once registers one handler to singles', t => {
+
+    let registry = Registry([]);
+    registry.once('enter', () => {});
+
+    t.true(registry.singles.enter.length === 1);
+
+});
+
+test('Registry.once handlers are removed after invocation', t => {
+
+    let registry = Registry([document.body]);
+    registry.once('enter', () => {})
+        .emit('enter', document.body);
+
+    t.true(!registry.singles.enter.length);
+
+});
