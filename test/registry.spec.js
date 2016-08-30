5807a4997fb651adba6665ccf9f7edf1517c86d8
Cam Wiegert <cam@camwiegert.com>
2016-08-30 11:38:58 -0500

    Add initial tests for Registry

--- test/registry.spec.js
+++ test/registry.spec.js
@@ -1,3 +1,12 @@
 import test from 'ava';
+import Registry from '../src/registry';
 
-test.todo('Write test for Registry');
+test('Registry returns a registry', t => {
+    let registry = Registry([document.body]);
+    t.deepEqual(registry, {
+        current: [],
+        elements: [document.body],
+        handlers: { enter: [], exit: [] },
+        singles: { enter: [], exit: [] }
+    });
+});
