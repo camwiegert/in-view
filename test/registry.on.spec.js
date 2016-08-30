96f7a62748acb739508d31109931291085d25997
Cam Wiegert <cam@camwiegert.com>
2016-08-30 12:00:33 -0500

    Add initial tests for Registry.on

--- test/registry.on.spec.js
+++ test/registry.on.spec.js
@@ -1,3 +1,17 @@
 import test from 'ava';
+import Registry from '../src/registry';
 
-test.todo('Write test for Registry.on');
+test('Registry.on registers one handler to handlers', t => {
+
+    let registry = Registry([]);
+
+    registry.on('enter', () => {});
+    t.true(registry.handlers.enter.length === 1);
+
+    registry.on('exit', () => {});
+    t.true(registry.handlers.exit.length === 1);
+
+    registry.on('enter', () => {});
+    t.true(registry.handlers.enter.length === 2);
+
+});
