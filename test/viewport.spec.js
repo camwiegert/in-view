ae3b5dbae429b93a4b2bb9a8149d60df9cc96b42
Cam Wiegert <cam@camwiegert.com>
2016-08-30 11:29:43 -0500

    Add initial tests for inViewport

--- test/viewport.spec.js
+++ test/viewport.spec.js
@@ -1,3 +1,26 @@
 import test from 'ava';
+import { inViewport } from '../src/viewport';
 
-test.todo('Write test for utils.inViewport');
+window.innerWidth = 1280;
+window.innerHeight = 700;
+
+let stub = {
+    getBoundingClientRect() {
+        return {
+            bottom: 232,
+            height: 108,
+            left: 196,
+            right: 1084,
+            top: 124,
+            width: 888
+        };
+    }
+};
+
+test('inViewport returns a boolean', t => {
+    t.true(typeof inViewport(stub) === 'boolean');
+});
+
+test('inViewport accepts an offset', t => {
+    t.false(inViewport(stub, 250));
+});
