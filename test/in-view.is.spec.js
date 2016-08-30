7cde01cca7d5c5a451c0344dc03c724f18b1e20d
Cam Wiegert <cam@camwiegert.com>
2016-08-30 12:57:00 -0500

    Add initial tests for inView.is

--- test/in-view.is.spec.js
+++ test/in-view.is.spec.js
@@ -1,3 +1,7 @@
 import test from 'ava';
+import inView from '../src/in-view';
+import { inViewport } from '../src/viewport';
 
-test.todo('Write test for inView.is');
+test('inView.is is a proxy for inViewport', t => {
+    t.deepEqual(inView.is, inViewport);
+});
