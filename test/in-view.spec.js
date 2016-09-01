a021cf5aa9f138e941f08b8c8afb54f02737874b
Dan Levy <dan@danlevy.net>
2016-09-01 12:38:04 -0600

    Test added for scroll parent container option

--- test/in-view.spec.js
+++ test/in-view.spec.js
@@ -2,6 +2,10 @@ import test from 'ava';
 import inView from '../src/in-view';
 import Registry from '../src/registry';
 
+const addDiv = (ctx = document.body) => {
+    ctx.appendChild(document.createElement('div'));
+};
+
 test('inView is a function', t => {
     t.true(typeof inView === 'function');
 });
@@ -16,18 +20,21 @@ test('inView returns existing registries', t => {
 });
 
 test('inView updates existing registry elements', t => {
-
-    const addDiv = () => {
-        document.body.appendChild(
-            document.createElement('div')
-        );
-    };
-
+    document.body.innerHTML = '';
     t.true(inView('div').elements.length === 0);
-
     addDiv();
     t.true(inView('div').elements.length === 1);
 
+});
+
+test('inView supports custom scroll container', t => {
+    document.body.innerHTML = '';
+    const scroller = document.createElement('section');
+    document.body.appendChild(scroller);
+    addDiv(scroller);
+    addDiv(scroller);
+    addDiv(scroller);
+    t.true(inView('div', scroller).elements.length === 3);
 });
 
 test.after(initBrowserEnv);
