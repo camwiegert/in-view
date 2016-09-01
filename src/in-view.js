090b941b97177ca8221ee027c3b0979d33e21870
Dan Levy <dan@danlevy.net>
2016-09-01 12:18:56 -0600

    Adding support for specific parent container/scroller

--- src/in-view.js
+++ src/in-view.js
@@ -1,77 +1,109 @@
-import Registry from './registry';
-import { inViewport } from './viewport';
-import { throttle } from 'lodash';
-
+import Registry         from './registry';
+import { inViewport }   from './viewport';
+import { throttle }     from 'lodash';
+import  WeakMap         from 'weakmap-shim';
 /**
 * Create and return the inView function.
 */
 const inView = () => {
 
-    // How often and on what events we should check each registry.
-    const threshold = 100;
-    const triggers  = ['scroll', 'resize', 'load'];
+  // How often and on what events we should check each registry.
+  const threshold = 100;
+  const triggers  = ['scroll', 'resize', 'load'];
+  const listeners = new WeakMap();
 
-    // By default, use an offset of 0.
-    let offset = 0;
+  // By default, use an offset of 0.
+  let offset = 0;
 
-    /**
-    * Maintain a hashmap of all registries and a history
-    * of selectors to enumerate.
-    */
-    let selectors = { history: [] };
+  /**
+  * Maintain a hashmap of all registries and a history
+  * of selectors to enumerate.
+  */
+  let selectors = { history: [] };
 
-    /**
-    * For each trigger event on window, add a listener
-    * which checks each registry, throttled to threshold.
-    */
-    triggers.forEach(event =>
-        addEventListener(event, throttle(() => {
-            selectors.history.forEach(selector => {
-                selectors[selector].check(offset);
-            });
-        }, threshold)));
+  // Const event handlers list, used to add/remove events
+  const handlers = triggers.map(event => {
+    return {
+      event,
+      handler: throttle(() => {
+        selectors.history.forEach(selector => {
+          selectors[selector].check(control.offset);
+        });
+      }, threshold)
+    }
+  });
 
-    /**
-    * The main interface. Take a selector and retrieve
-    * the associated registry or create a new one.
-    */
-    let control = (selector) => {
+  /**
+  * For each trigger event on window, add a listener
+  * which checks each registry, throttled to threshold.
+  */
+  const addListeners = (container) => {
+    handlers.forEach(({event, handler}) => container.addEventListener(event, handler));
+    return handlers;
+  }
 
-        if (typeof selector !== 'string') return;
+  const removeListeners = (container) => {
+    handlers.forEach(({event, handler}) => container.removeEventListener(event, handler));
+    return handlers;
+  }
 
-        // Get an up-to-date list of elements.
-        let elements = [].slice.call(document.querySelectorAll(selector));
+  /**
+  * The main interface. Take a selector and retrieve
+  * the associated registry or create a new one.
+  */
+  let control = (selector, container = window) => {
 
-        // If the registry exists, update the elements.
-        if (selectors.history.indexOf(selector) > -1) {
-            selectors[selector].elements = elements;
-        }
+    if (typeof selector !== 'string') return;
 
-        // If it doesn't exist, create a new registry.
-        else {
-            selectors[selector] = Registry(elements);
-            selectors.history.push(selector);
-        }
+    // check for events on the container (should handle if the container changes - eg drag-drop ui, multi-scroll, page/region checks for different item types)
+    if (listeners.has(container) !== true) {
+      listeners.set(container, addListeners(container));
+    }
 
-        return selectors[selector];
-    };
+    // Get an up-to-date list of elements.
+    let elements = [].slice.call(document.querySelectorAll(selector));
 
-    /**
-    * Add a static offset() method to update
-    * the offset.
-    */
-    control.offset = n => {
-        return (typeof n === 'number') ?
-            offset = n :
-            offset;
-    };
+    // If the registry exists, update the elements.
+    if (selectors.history.indexOf(selector) > -1) {
+      selectors[selector].elements = elements;
+    }
+    // If it doesn't exist, create a new registry.
+    else {
+      selectors[selector] = Registry(elements);
+      selectors.history.push(selector);
+    }
 
-    /**
-    * Add a static is() method to the main interface
-    * and return it.
-    */
-    control.is = inViewport;
-    return control;
+    return selectors[selector];
+  };
+
+  /**
+  * Add a static offset() method to update
+  * the offset.
+  */
+  control.offset = n => {
+    return (typeof n === 'number') ?
+      offset = n :
+      offset;
+  };
+
+  /**
+  * Add a static is() method to the main interface
+  * and return it.
+  */
+  control.is = inViewport;
+
+  /**
+   * Add static destroy method
+   */
+  control.destroy = (container = window) => {
+    if (listeners.has(container)) {
+      listeners.delete(container);
+      return removeListeners(container);
+    }
+  }
+
+
+  return control;
 
 };
 
