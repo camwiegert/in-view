c58fed73a1fd80c7ae0f7decaaafd887a0f664cb
Konrad Mohrfeldt <mohrfeldt@silicann.com>
2016-09-07 17:04:17 +0200

    make the inViewport check configurable in registries

--- test/registry.spec.js
+++ test/registry.spec.js
@@ -2,11 +2,13 @@ import test from 'ava';
 import Registry from '../src/registry';
 
 test('Registry returns a registry', t => {
-    let registry = Registry([document.body]);
+    let inViewport = () => {};
+    let registry = Registry([document.body], inViewport);
     t.deepEqual(registry, {
         current: [],
         elements: [document.body],
         handlers: { enter: [], exit: [] },
-        singles: { enter: [], exit: [] }
+        singles: { enter: [], exit: [] },
+        inViewport
     });
 });
