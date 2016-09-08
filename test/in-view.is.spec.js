ac70f2ff94d0e57b1bfb427f5db9c4f842fc4400
Cam Wiegert <cam@camwiegert.com>
2016-09-08 10:05:45 -0500

    Update inView.is test to ensure boolean

--- test/in-view.is.spec.js
+++ test/in-view.is.spec.js
@@ -2,6 +2,18 @@ import test from 'ava';
 import inView from '../src/in-view';
 import { inViewport } from '../src/viewport';
 
-test('inView.is is a proxy for inViewport', t => {
-    t.deepEqual(inView.is, inViewport);
+const stub = {
+    getBoundingClientRect() {
+        return {
+            top: 50,
+            right: 50,
+            bottom: 50,
+            left: 50
+        };
+    }
+};
+
+test('inView.is returns a boolean', t => {
+    t.true(typeof inView.is(stub) === 'boolean');
+    t.true(inView.is(stub));
 });
