924c4e1e3d3f251d54165b69e7ef8e868381aa86
Dan Levy <dan@danlevy.net>
2016-09-23 22:39:25 -0600

    WIP: adding container support extending viewport tests

--- src/viewport.js
+++ src/viewport.js
@@ -2,13 +2,23 @@
 * Check whether an element is in the viewport by
 * more than offset px.
 */
-export function inViewport (element, offset = 0) {
-
-    let { top, right, bottom, left } = element.getBoundingClientRect();
-
+export function inViewport (el, container = window, offset = 0) {
+    if (typeof offset === 'object' && Number.isNumeric(container)) {
+        // out of order, auto shift last 2 params
+        [offset, container] = [container, offset];
+    }
+    const containerBox    = container    && container.body     ? getElemBox(container) : null;
+    const containerWidth  = containerBox ? containerBox.width  : container.innerWidth;
+    const containerHeight = containerBox ? containerBox.height : container.innerHeight;
+    const { top, right, bottom, left } = getElemBox(el);
     return bottom > offset
         && right > offset
-        && window.innerWidth - left > offset
-        && window.innerHeight - top > offset;
+        && containerWidth - left > offset
+        && containerHeight - top > offset;
+}
 
+function getElemBox(el) {
+    if (!el) { return {}; }
+    let { top, right, bottom, left } = el.getBoundingClientRect();
+    return { top, right, bottom, left };
 }
--- test/viewport.spec.js
+++ test/viewport.spec.js
@@ -1,26 +1,80 @@
-import test from 'ava';
-import { inViewport } from '../src/viewport';
+import test                     from 'ava';
+import { example1, example2,
+    example3, example4 }        from './helpers/stub-nodes';
+import { inViewport }           from '../src/viewport';
 
-window.innerWidth = 1280;
+window.innerWidth  = 1280;
 window.innerHeight = 700;
 
-let stub = {
-    getBoundingClientRect() {
-        return {
-            bottom: 232,
-            height: 108,
-            left: 196,
-            right: 1084,
-            top: 124,
-            width: 888
-        };
-    }
-};
 
-test('inViewport returns a boolean', t => {
-    t.true(typeof inViewport(stub) === 'boolean');
+test('inViewport: #example1 returns a boolean', t => {
+    t.true(typeof inViewport(example1) === 'boolean');
 });
 
-test('inViewport accepts an offset', t => {
-    t.false(inViewport(stub, 250));
+test('inViewport: #example1 accepts an offset', t => {
+    t.false(inViewport(example1, 250));
 });
+
+// TEST example2
+// See ascii art of example2
+test('inViewport: #example2 handles parent container w/o offset', t => {
+    t.true(inViewport(example2, example2.parentNode));
+});
+
+test('inViewport: #example2 handles parent container w/ offset', t => {
+    t.true(inViewport(example2, example2.parentNode, 40));
+});
+
+test('inViewport: #example2 handles parent container w/ too much offset', t => {
+    t.false(inViewport(example2, example2.parentNode, 60));
+});
+
+
+// TEST example3
+// See ascii art of example3
+test('inViewport: #example3 handles parent container w/o offset', t => {
+    t.true(inViewport(example3, example3.parentNode));
+});
+
+test('inViewport: #example3 handles parent container w/ offset', t => {
+    t.true(inViewport(example3, example3.parentNode, 40));
+});
+
+test('inViewport: #example3 handles parent container w/ too much offset', t => {
+    t.true(inViewport(example3, example3.parentNode, 60));
+});
+
+// TEST example4
+// See ascii art of example4
+test('inViewport: #example4 handles parent container w/o offset', t => {
+    t.false(inViewport(example4, example4.parentNode));
+});
+
+test('inViewport: #example4 handles parent container w/ offset', t => {
+    t.false(inViewport(example4, example4.parentNode, 40));
+});
+
+test('inViewport: #example4 handles parent container w/ too much offset', t => {
+    t.false(inViewport(example4, example3.parentNode, 60));
+});
+
+
+// Stub data: example2
+//  A Not-to-scale Example of stub-nodes .example2
+//    |==========|
+//    |          |
+//    |          |
+//    |          |
+// ===|==========|===========================================
+// || |          |                                         ||
+// || |          |                                         ||
+// || |==========|                                         ||
+// ||                                                      ||
+// ||                                                      ||
+// ||                                                      ||
+// ||                                                      ||
+// ||                                                      ||
+// ||                                                      ||
+// ||                                                      ||
+// ||                                                      ||
+
