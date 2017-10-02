dbdc406976994f2748a49550d3148ee1fad3c30e
Alfredo Lopez <lfre@users.noreply.github.com>
2017-10-02 17:42:59 -0400

    Added test for validate

---
+++ test/registry.validate.spec.js
@@ -0,0 +1,18 @@
+import test from 'ava';
+import { merge } from 'lodash';
+import { Registry, defaults } from '../src/registry';
+
+const registry = Registry([]);
+
+test('registry.validate returns defaults for non objects', t => {
+    t.deepEqual(registry.validate(), defaults);
+});
+
+test('registry.validate merges properties', t => {
+	let output = merge({}, defaults, {
+		threshold: 0.6
+	});
+    t.deepEqual(registry.validate({
+    	threshold: 0.6
+    }), output);
+});
