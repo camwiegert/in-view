dd2ba7793fbb61486b463eb7f2bbf000c1dc1bb9
Jeff <jeff.sagal@briteweb.com>
2018-02-14 13:16:11 -0800

    Remove Absolute Positioning
    - use transforms to position dots, more performant
    - use psuedo element for dot appearance to avoid conflict with JS transforms

--- docs/lib/js/main.js
+++ docs/lib/js/main.js
@@ -3,16 +3,24 @@ import inView from '../../../src';
 let count = 200;
 let field = document.querySelector('.field');
 
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const createDot = () => {
-    let dot = document.createElement('div');
-    dot.className = 'dot';
-    dot.style.top = `${(Math.random() * 100)}%`;
-    dot.style.left = `${(Math.random() * 100)}%`;
-    return dot;
+  let dot = document.createElement('div');
+  dot.className = 'dot';
+
+  const translateX = getRandomInt(0, field.offsetWidth);
+  const translateY = getRandomInt(0, field.offsetHeight);
+
+  dot.style.transform = `translate(${translateX}px, ${translateY}px)`;
+
+  return dot;
 }
 
 while (count--) {
-    field.appendChild(createDot());
+  field.appendChild(createDot());
 }
 
 inView.offset(50);
